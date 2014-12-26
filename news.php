0f73140b9f29e999c68284e7015a8fd2db745afc
fede1608 <fedecasares1993@yahoo.com.ar>
2014-12-26 13:58:23 -0300

    Avatar en news

--- news.php
+++ news.php
@@ -18,13 +18,15 @@ try {
     if ($_GET['tid']) {
         $response["success"] = 1;
         $news = $MyBBI->getPosts(array(
-            'fields' => 'tid,subject,username,message,dateline',
+            'fields' => 'tid,subject,username,uid,message,dateline',
             'order_by' => 'dateline',
             'order_dir' => 'DESC',
             'limit_start' => 0,
             'limit' => 1,
-            'where' => "fid=2 AND tid=" . $_GET['tid']));
+            'where' => "fid=2 AND replyto=0 AND tid=" . $_GET['tid']));
+        $a = $MyBBI->getUser($news[0]['uid']);
         $response['news'] = $news[0];
+        $response['news']['avatar'] = "http://minekkit.com/foro/" . $a['avatar'];
     } else {
         $response["success"] = 1;
         $response['posts'] = $MyBBI->getPosts(array(
@@ -33,7 +35,7 @@ try {
             'order_dir' => 'DESC',
             'limit_start' => 0,
             'limit' => $_POST['limit'] ? $_POST['limit'] : 10,
-            'where' => "fid=2"));
+            'where' => "fid=2 AND replyto=0"));
     }
 } catch (Exception $e) {
     $response["success"] = 0;
