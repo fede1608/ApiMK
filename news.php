ebc0966b1eeeef1686baadb969c9a81c1c0d4966
fede1608 <fedecasares1993@yahoo.com.ar>
2014-12-26 03:03:39 -0300

    Api de Noticias - Single news

--- news.php
+++ news.php
@@ -15,14 +15,26 @@ require_once '../foro/global.php';
 $MyBBI = new MyBBIntegrator($mybb, $db, $cache, $plugins, $lang, $config);
 $response["success"] = 0;
 try {
-    $response["success"] = 1;
-    $response['posts'] = $MyBBI->getPosts(array(
-        'fields' => 'tid,subject,username,message,dateline',
-        'order_by' => 'dateline',
-        'order_dir' => 'DESC',
-        'limit_start' => 0,
-        'limit' => $_POST['limit'] ? $_POST['limit'] : 10,
-        'where' => "fid=2"));
+    if ($_GET['tid']) {
+        $response["success"] = 1;
+        $news = $MyBBI->getPosts(array(
+            'fields' => 'tid,subject,username,message,dateline',
+            'order_by' => 'dateline',
+            'order_dir' => 'DESC',
+            'limit_start' => 0,
+            'limit' => 1,
+            'where' => "fid=2 AND tid=" . $_GET['tid']));
+        $response['news'] = $news[0];
+    } else {
+        $response["success"] = 1;
+        $response['posts'] = $MyBBI->getPosts(array(
+            'fields' => 'tid,subject,username,message,dateline',
+            'order_by' => 'dateline',
+            'order_dir' => 'DESC',
+            'limit_start' => 0,
+            'limit' => $_POST['limit'] ? $_POST['limit'] : 10,
+            'where' => "fid=2"));
+    }
 } catch (Exception $e) {
     $response["success"] = 0;
     $response["error"] = $e->getMessage();
